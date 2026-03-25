9cccd29746d939a4b08525ffaeef3bb61614ec66
Rodrigo Yuji Seto Soma <rodrigo.y.seto@gmail.com>
2026-03-25 20:26:39 +0900

    feat: Initialize React application with basic structure, components, and dependencies for a WhatsApp clone.

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import ChatListItem from "./components/ChatListItem";
+
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
+
+export default () => {
+
+  const [chatList, setChatList] = useState([{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}]);
+
+  return (
+    <div className="app-window">
+      <div className="sidebar">
+        <header>
+          <img className="header-avatar" src="https://www.w3schools.com/w3images/avatar2.png" alt="" />
+          <div className="header-buttons">
+            <div className="header-btn">
+              <DonutLargeIcon style={{ color: '#919191' }} />
+            </div>
+            <div className="header-btn">
+              <ChatIcon style={{ color: '#919191' }} />
+            </div>
+            <div className="header-btn">
+              <MoreVertIcon style={{ color: '#919191' }} />
+            </div>
+          </div>
+        </header>
+
+        <div className="search">
+          <div className="search-input">
+            <SearchIcon style={{ color: '#919191' }} />
+            <input type="search" placeholder="Search or start new chat" />
+          </div>
+            
+        </div>
+
+        <div className="chatlist">
+          {chatList.map((item, key) => (
+            <ChatListItem key={key}/>
+          ))}
+        </div>
+
+      </div>
+      <div className="contentarea">
+        ...
+      </div>
+    </div>
+  );
+};
---
+++ src/components/ChatListItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './ChatListItem.css';
+
+export default () => {
+  return (
+    <div className="chatListItem">
+      <img className="chatListItem--avatar" src="https://www.w3schools.com/w3images/avatar2.png" alt="" />
+      <div className="chatListItem--lines">
+        <div className="chatListItem--line">
+          <div className="chatListItem--name">Contact Name</div>
+          <div className="chatListItem--date">10:00</div>
+        </div>
+        <div className="chatListItem--line">
+          <div className="chatListItem--lastMessage">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
